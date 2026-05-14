58948e33d3c6632e6b4c7617356d2a01dec3fd3d
刘颖 <1452970202@qq.com>
2026-05-14 18:12:40 +0800

    修复邮箱验证字符

--- web/src/view/superAdmin/user/user.vue
+++ web/src/view/superAdmin/user/user.vue
@@ -513,7 +513,7 @@
     ],
     email: [
       {
-        pattern: /^([0-9A-Za-z\-_.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g,
+        pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
         message: '请输入正确的邮箱',
         trigger: 'blur'
       }
