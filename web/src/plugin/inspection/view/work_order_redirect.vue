38a2949e58a5a657a27c67856e657adcd797eb31
刘颖 <1452970202@qq.com>
2026-05-19 15:00:51 +0800

    不跳转

--- web/src/plugin/inspection/view/work_order_redirect.vue
+++ web/src/plugin/inspection/view/work_order_redirect.vue
@@ -6,6 +6,6 @@
 import { onMounted } from 'vue'
 onMounted(() => {
   const url = `${window.location.origin}${window.location.pathname}#/inspectWorkOrder`
-  window.open(url, '_self')
+  window.open(url, '_blank')
 })
 </script>
